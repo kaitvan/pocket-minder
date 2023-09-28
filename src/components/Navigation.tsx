bb4b1e947b4f40a8b1e3da17595b68c5957bee17
Kaitlyn Van Gorkom <kaitlyn.vangorkom@gmail.com>
2023-09-28 18:58:39 -0500

    Set up basic nav bar with sign in and out button

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -1,8 +1,38 @@
+import { useState } from "react";
+
+import { Link } from "react-router-dom";
+import Logo from "../assets/logo.png";
+import styles from "./Navigation.module.css";
+
 const Navigation = () => {
-  return (
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+
+  const signInHandler = () => {
+    setIsAuthenticated(true);
+  };
+
+  const signOutHandler = () => {
+    setIsAuthenticated(false);
+  };
+
+  const fullNavBar = (
     <>
-      <h1>Navigation</h1>
+      <Link to="/dashboard">Dashboard</Link>
+      <Link to="/log">Log</Link>
+      <Link to="/progress">Progress</Link>
+      <button onClick={signOutHandler}>Sign Out</button>
     </>
+  );
+
+  return (
+    <header>
+      <img src={Logo} alt="Pocket minder logo" />
+      <Link to="/" className={styles["pocket-minder"]}>
+        Pocket Minder
+      </Link>
+      {isAuthenticated && fullNavBar}
+      {!isAuthenticated && <button onClick={signInHandler}>Sign In</button>}
+    </header>
   );
 };
 
