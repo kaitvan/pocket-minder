8b980e558affd5fe7c8253549f6fc7129d00561c
Kaitlyn Van Gorkom <kaitlyn.vangorkom@gmail.com>
2023-09-28 18:17:11 -0500

    Remove initial JSX and add logo with title

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,140 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
 
 function App() {
-  const [count, setCount] = useState(0)
+  // interface User {
+  //   username: string;
+  //   password: string;
+  //   firstName: string;
+  //   lastName: string;
+  //   userCreatedDate: Date;
+  // }
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  // async function addUserHandler(user: User): Promise<void> {
+  //   const response = await fetch(
+  //     "https://pocket-minder-d506a-default-rtdb.firebaseio.com/users.json",
+  //     {
+  //       method: "POST",
+  //       body: JSON.stringify(user),
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //     }
+  //   );
+  //   const data = await response.json();
+  //   console.log(data);
+  // }
+
+  // addUserHandler({
+  //   username: "kaitvan",
+  //   password: "12345678",
+  //   firstName: "Kaitlyn",
+  //   lastName: "Van Gorkom",
+  //   userCreatedDate: new Date(),
+  // });
+
+  // interface Category {
+  //   title: string;
+  //   monthlyAmount: number;
+  //   icon: string;
+  //   color: string;
+  //   userKey: string;
+  // }
+
+  // async function addCategoryHandler(category: Category): Promise<void> {
+  //   const response = await fetch(
+  //     "https://pocket-minder-d506a-default-rtdb.firebaseio.com/categories.json",
+  //     {
+  //       method: "POST",
+  //       body: JSON.stringify(category),
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //     }
+  //   );
+  //   const data = await response.json();
+  //   console.log(data);
+  // }
+
+  // addCategoryHandler({
+  //   title: "Auto & Transport",
+  //   monthlyAmount: 160,
+  //   icon: "car",
+  //   color: "F54B89",
+  //   userKey: "-Nf9G7j1HcC25cUekB3Y",
+  // });
+
+  // addCategoryHandler({
+  //   title: "Food & Drinks",
+  //   monthlyAmount: 200,
+  //   icon: "utensils",
+  //   color: "56BEBA",
+  //   userKey: "-Nf9G7j1HcC25cUekB3Y",
+  // });
+
+  // addCategoryHandler({
+  //   title: "Fun & Recreation",
+  //   monthlyAmount: 100,
+  //   icon: "umbrella",
+  //   color: "FEAB03",
+  //   userKey: "-Nf9G7j1HcC25cUekB3Y",
+  // });
+
+  // addCategoryHandler({
+  //   title: "Education",
+  //   monthlyAmount: 150,
+  //   icon: "graduate",
+  //   color: "516DF5",
+  //   userKey: "-Nf9G7j1HcC25cUekB3Y",
+  // });
+
+  // addCategoryHandler({
+  //   title: "Personal Care",
+  //   monthlyAmount: 80,
+  //   icon: "heart-shield",
+  //   color: "9BC53D",
+  //   userKey: "-Nf9G7j1HcC25cUekB3Y",
+  // });
+
+  // addCategoryHandler({
+  //   title: "Household Goods",
+  //   monthlyAmount: 60,
+  //   icon: "home",
+  //   color: "F54B89",
+  //   userKey: "-Nf9G7j1HcC25cUekB3Y",
+  // });
+
+  // interface Expense {
+  //   date: Date;
+  //   payee: string;
+  //   amount: number;
+  //   categoryKey: string;
+  //   userKey: string;
+  // }
+
+  // async function addExpenseHandler(expense: Expense): Promise<void> {
+  //   const response = await fetch(
+  //     "https://pocket-minder-d506a-default-rtdb.firebaseio.com/expenses.json",
+  //     {
+  //       method: "POST",
+  //       body: JSON.stringify(expense),
+  //       headers: {
+  //         "Content-Type": "application/json",
+  //       },
+  //     }
+  //   );
+  //   const data = await response.json();
+  //   console.log(data);
+  // }
+
+  // addExpenseHandler({
+  //   date: new Date(),
+  //   payee: "H-E-B Gas",
+  //   amount: 55.25,
+  //   categoryKey: "-Nf9IUCXGYB9Kcku_oAC",
+  //   userKey: "-Nf9G7j1HcC25cUekB3Y",
+  // });
+
+  return <></>;
 }
 
-export default App
+export default App;
