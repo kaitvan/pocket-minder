f1f4df30dfac6362c6e3eac42533a788a46999c0
Kaitlyn Van Gorkom <kaitlyn.vangorkom@gmail.com>
2023-09-28 19:51:47 -0500

    Add styling for navbar

--- src/components/Navigation.tsx
+++ src/components/Navigation.tsx
@@ -15,23 +15,51 @@ const Navigation = () => {
     setIsAuthenticated(false);
   };
 
-  const fullNavBar = (
-    <>
-      <Link to="/dashboard">Dashboard</Link>
-      <Link to="/log">Log</Link>
-      <Link to="/progress">Progress</Link>
-      <button onClick={signOutHandler}>Sign Out</button>
-    </>
+  const isAuthenticatedNav = (
+    <nav>
+      <ul className={styles.list}>
+        <li>
+          <Link to="/dashboard">Dashboard</Link>
+        </li>
+        <li>
+          <Link to="/log">Log</Link>
+        </li>
+        <li>
+          <Link to="/progress">Progress</Link>
+        </li>
+        <li>
+          <button onClick={signOutHandler} className={styles["auth-button"]}>
+            Sign Out
+          </button>
+        </li>
+      </ul>
+    </nav>
+  );
+
+  const isNotAuthenticatedNav = (
+    <nav>
+      <ul className={styles.list}>
+        <li>
+          <button onClick={signInHandler} className={styles["auth-button"]}>
+            Sign In
+          </button>
+        </li>
+      </ul>
+    </nav>
   );
 
   return (
-    <header>
-      <img src={Logo} alt="Pocket minder logo" />
-      <Link to="/" className={styles["pocket-minder"]}>
-        Pocket Minder
-      </Link>
-      {isAuthenticated && fullNavBar}
-      {!isAuthenticated && <button onClick={signInHandler}>Sign In</button>}
+    <header className={styles.header}>
+      <div className={styles.logo}>
+        <img src={Logo} alt="Pocket minder logo" />
+        <Link to="/" className={styles["pocket-minder"]}>
+          Pocket Minder
+        </Link>
+      </div>
+      <div className={styles.list}>
+        {isAuthenticated && isAuthenticatedNav}
+        {!isAuthenticated && isNotAuthenticatedNav}
+      </div>
     </header>
   );
 };
