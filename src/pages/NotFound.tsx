58c655e289dd665ba12765333d95621ec74d175e
Kaitlyn Van Gorkom <kaitlyn.vangorkom@gmail.com>
2023-09-28 21:56:21 -0500

    Style not found page

--- src/pages/NotFound.tsx
+++ src/pages/NotFound.tsx
@@ -1,7 +1,24 @@
+import Navigation from "../components/Navigation";
+import Footer from "../components/Footer";
+import styles from "./NotFound.module.css";
+import Search from "../assets/search.png";
+
 const NotFound = () => {
   return (
     <>
-      <h1>NotFound</h1>
+      <Navigation />
+      <main className={styles.main}>
+        <div className={styles.text}>
+          <h1 className={styles.h1}>Oops!</h1>
+          <p>We can't seem to find the page you're looking for.</p>
+        </div>
+        <img
+          src={Search}
+          alt="Illustrated person searching on a laptop"
+          className={styles["search-img"]}
+        />
+      </main>
+      <Footer />
     </>
   );
 };
