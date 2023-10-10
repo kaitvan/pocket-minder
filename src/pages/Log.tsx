26846a9eeabfa5a24cb54173fd451ad82cf83b87
Kaitlyn Van Gorkom <kaitlyn.vangorkom@gmail.com>
2023-10-10 11:03:24 -0500

    Add basic styling on expense log

--- src/pages/Log.tsx
+++ src/pages/Log.tsx
@@ -1,8 +1,36 @@
+import styles from "./Log.module.css";
+
 const Log = () => {
   return (
-    <>
-      <h1>Log</h1>
-    </>
+    <div className={styles.container}>
+      <div className={styles.header}>
+        <h1>Expense Log</h1>
+        <button className={styles["add-expense-btn"]}>Add Expense</button>
+      </div>
+      <div className={styles.log}>
+        <div>
+          <div className={styles.filter}></div>
+          <div className={styles.search}></div>
+        </div>
+        <hr></hr>
+        <div>
+          <table>
+            <tr>
+              <th>Date</th>
+              <th>Payee</th>
+              <th>Category</th>
+              <th>Amount</th>
+            </tr>
+            <tr>
+              <td>09-23-23</td>
+              <td>Home Depot</td>
+              <td>Household Goods</td>
+              <td>$28.53</td>
+            </tr>
+          </table>
+        </div>
+      </div>
+    </div>
   );
 };
 
