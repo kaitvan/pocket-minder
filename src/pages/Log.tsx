bda7b77e28a4b0f9a06cc574a95d5e5db1342af7
Kaitlyn Van Gorkom <kaitlyn.vangorkom@gmail.com>
2023-10-11 17:35:15 -0500

    Fetch categories and expenses from firebase

--- src/pages/Log.tsx
+++ src/pages/Log.tsx
@@ -1,6 +1,82 @@
+import { useEffect, useState } from "react";
 import styles from "./Log.module.css";
 
+interface Expense {
+  id: string;
+  date: string;
+  payee: string;
+  category: string;
+  amount: number;
+}
+
+interface Category {
+  id: string;
+  icon: string;
+  monthlyAmount: Number;
+  title: string;
+}
+
 const Log = () => {
+  const [expenses, setExpenses] = useState<Array<Expense>>([]);
+  const [categories, setCategories] = useState<Array<Category>>([]);
+
+  const getCategory = (categoryKey: string): Category | undefined => {
+    let category = categories.find((category) => categoryKey === category.id);
+    return category;
+  };
+
+  const getExpenses = () => {
+    fetch(
+      "https://pocket-minder-d506a-default-rtdb.firebaseio.com/expenses.json",
+      {
+        method: "GET",
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const loadedExpenses = [];
+        for (const key in data) {
+          loadedExpenses.push({
+            id: key,
+            date: data[key].date,
+            payee: data[key].payee,
+            category: getCategory(data[key].categoryKey)?.title || "Unknown",
+            amount: data[key].amount,
+          });
+        }
+
+        setExpenses(loadedExpenses);
+      });
+  };
+
+  const getCategories = () => {
+    fetch(
+      "https://pocket-minder-d506a-default-rtdb.firebaseio.com/categories.json",
+      {
+        method: "GET",
+      }
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        const loadedCategories = [];
+        for (const key in data) {
+          loadedCategories.push({
+            id: key,
+            icon: data[key].icon,
+            monthlyAmount: data[key].monthlyAmount,
+            title: data[key].title,
+          });
+        }
+
+        setCategories(loadedCategories);
+      });
+  };
+
+  useEffect(() => {
+    getCategories();
+    getExpenses();
+  });
+
   return (
     <div className={styles.container}>
       <div className={styles.header}>
@@ -8,25 +84,54 @@ const Log = () => {
         <button className={styles["add-expense-btn"]}>Add Expense</button>
       </div>
       <div className={styles.log}>
-        <div>
-          <div className={styles.filter}></div>
-          <div className={styles.search}></div>
+        <div className={styles.filters}>
+          <div className={styles.filter}>
+            <form autoComplete="off">
+              <div className={styles["custom-select"]}>
+                <select>
+                  <option value="">Filter By Category</option>
+                  <option value="auto">Auto & Transport</option>
+                  <option value="food">Food & Drinks</option>
+                  <option value="fun">Fun & Recreation</option>
+                  <option value="education">Education</option>
+                  <option value="personal">Personal Care</option>
+                  <option value="household">Household Goods</option>
+                </select>
+              </div>
+            </form>
+          </div>
+          <div className={styles.search}>
+            <form autoComplete="off">
+              <div className={styles["custom-input"]}>
+                <input type="text" id="search" name="search"></input>
+              </div>
+            </form>
+          </div>
         </div>
         <hr></hr>
         <div>
           <table>
-            <tr>
-              <th>Date</th>
-              <th>Payee</th>
-              <th>Category</th>
-              <th>Amount</th>
-            </tr>
-            <tr>
-              <td>09-23-23</td>
-              <td>Home Depot</td>
-              <td>Household Goods</td>
-              <td>$28.53</td>
-            </tr>
+            <thead>
+              <tr>
+                <th>Date</th>
+                <th>Payee</th>
+                <th>Category</th>
+                <th>Amount</th>
+              </tr>
+            </thead>
+            <tbody>
+              {expenses.map((expense) => (
+                <tr key={expense.id}>
+                  <td>{`${expense.date.slice(5, 7)}-${expense.date.slice(
+                    8,
+                    10
+                  )}-${expense.date.slice(0, 4)}`}</td>
+                  <td>{expense.payee}</td>
+                  <td>{expense.category}</td>
+                  <td>${Number(expense.amount).toFixed(2)}</td>
+                </tr>
+              ))}
+            </tbody>
           </table>
         </div>
       </div>
